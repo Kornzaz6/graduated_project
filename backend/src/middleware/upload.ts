68ff74fc04a097bcd234417e76b8d20b9a0ed163
kornzaz6 <gonkawee123@gmail.com>
2026-03-11 17:41:35 +0700

    final 27 11-03-2026 fix verify function4

--- backend/src/middleware/upload.ts
+++ backend/src/middleware/upload.ts
@@ -1,35 +1,8 @@
 import multer from "multer"
-import path from "path"
-import fs from "fs"
-
-/* ================= UPLOAD PATH ================= */
-
-const uploadPath = path.resolve(process.cwd(), "uploads/slips")
-
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath, { recursive: true })
-}
 
 /* ================= STORAGE ================= */
 
-const storage = multer.diskStorage({
-
-  destination: (_req, _file, cb) => {
-    cb(null, uploadPath)
-  },
-
-  filename: (_req, file, cb) => {
-
-    const uniqueName =
-      Date.now() + "-" + Math.round(Math.random() * 1e9)
-
-    const ext = path.extname(file.originalname).toLowerCase()
-
-    cb(null, uniqueName + ext)
-
-  }
-
-})
+const storage = multer.memoryStorage()
 
 /* ================= FILE FILTER ================= */
 
@@ -60,7 +33,7 @@ export const uploadSlipMiddleware = multer({
   storage,
 
   limits: {
-    fileSize: 5 * 1024 * 1024
+    fileSize: 5 * 1024 * 1024 // 5MB
   },
 
   fileFilter
